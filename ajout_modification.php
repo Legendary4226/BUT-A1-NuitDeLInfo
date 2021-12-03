99a7522febfbe57952a0dca61c5d6aacb012133d
Vincent Chavot--Dambrun <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2021-12-03 02:02:29 +0100

    suppression de accueil.html+ accueil mis sous php+ajout de la partie html et php de la partie ajout donnés

---
+++ ajout_modification.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style/index.css">
+        <link rel="stylesheet" href="style/accueil.css">
+        <link rel="icon" href="style/images/Logo_Vyraj_80x80.png" />
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+        <?php
+            include("parts/modifier_ajouter.html");
+        ?>
+        </main>
+        <div>
+
+        </div>
+
+    </body>
+</html>
